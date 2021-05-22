2b25581c316ab51511cf9545683f958db12ded44
Brian Vargo <iconoclasthero@gmail.com>
2021-05-22 12:14:21 -0400

    Updated install.sh

--- ignore.sh
+++ ignore.sh
@@ -13,7 +13,7 @@ fi
 
 printf "Ignoring current song: "
 mpc -f %file%
-mpc current -f %file% >> "$pldir".mpdignore.m3u
+mpc current -f %file% >> "$pldir"/.mpdignore.m3u
 
 read -r -p "Do you wish to skip the current song? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mpdconf=/etc/mpd.conf
+installdir=/usr/local/sbin
 
 if [[ -f "$mpdconf" ]]			# defines the location of your mpd logfile path; this is defined in the mpd.conf file found at /etc/mpd.conf by default
  then
@@ -12,6 +13,15 @@ if [[ -f "$mpdconf" ]]			# defines the location of your mpd logfile path; this i
 #   mpdpass="Passwword123"		# manually code the location if /etc/mpd.conf doesn't exist.
 fi
 
-cp ./ignore.sh ./skip.sh ./mpdignore.sh /usr/local/sbin
+cp ./ignore.sh ./skip.sh ./mpdignore.sh "$installdir"
+chmod +X "$installdir/ignore.sh" "$installdir/skip.sh"
+
+if [[ ! -L "$installdir/skip" ]]; then
+ ln -s "$installdir/skip.sh" "$installdir/skip"
+fi
+
+if [[ ! -L  "$installdir/ignore" ]]; then
+ ln -s "$installdir/ignore.sh" "$installdir/ignore"
+fi
 
 sed -i "s/Passwword123/$mpdpass/g" /usr/local/sbin/skip.sh
