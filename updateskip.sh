8d8b9961b48c12f60b91a2e80217369532dbc6bf
iconoclast hero <iconoclasthero@gmail.com>
2024-03-25 15:29:43 -0400

    update

--- updateskip.sh
+++ updateskip.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
-
 mpdconf=/etc/mpd.conf
-
 scriptname=$(realpath "$0")
 
-function editscript(){ if [ "$1" = "edit" ] || [ "$1" = "nano" ]; then (/usr/bin/nano "$scriptname"); exit; fi ; }
+. mpdignore.functions
 
-function pause(){ read -p "$*" ; }
-
-editscript "$1"
-
-if [[ -f "$mpdconf" ]]			# defines the location of your mpd logfile path; this is defined in the mpd.conf file found at /etc/mpd.conf by default
- then
-   mpdpass=$(grep -v "^#" "$mpdconf" | grep -v "^$" | grep password | grep control | head -n 1)
-   mpdpass="${mpdpass%*\"}"
-   mpdpass="${mpdpass#*\"}"
-   mpdpass="${mpdpass%\@*}"
- else
-   read -p "What is the mpd controll password?" mpdpass
-#   mpdpass="Passwword123"		# manually code the location if /etc/mpd.conf doesn't exist.
-fi
+getmpdpass
 
 sudo cp ~/bin/gits/mpdignore/skip.sh /usr/local/sbin/skip.sh
 sudo sed -i "s/Passwword123/$mpdpass/" /usr/local/sbin/skip.sh
