cf83ec85d1852c86f1905922c18deb4e863bebdf
iconoclast hero <iconoclasthero@gmail.com>
2024-09-17 10:33:42 -0400

    update

--- skip.sh
+++ skip.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # NB: This script requires that the user have access to edit /var/log/mpd/mpd.log which means modifying the permissions/ownership
 
-#mpdconf="/etc/mpd.conf"
-
 #logsong() {
 #   i=$(date '+%b %d %H:%M : player: skipped ')
 #   j=$(mpcp current -f %file%)
@@ -10,10 +8,10 @@
 #}
 
 #mpcp() {
-#    command mpc -P Passwword123 "$@"
+#  mpc -P "$mpdpass" -h "$mpdhost" "$@"
 #}
 
-#if [[ -f "$mpdconf" ]]	# defines the location of your mpd logfile path; this is defined in the mpd.conf file found at /etc/mpd.conf by default
+#if [[ -f "$mpdconf" ]] # defines the location of your mpd logfile path; this is defined in the mpd.conf file found at  /etc/mpd.conf by default
 # then
 #   mpdlog=$(grep -v "^#" "$mpdconf" | grep -v "^$" | grep log_file)
 #   mpdlog="${mpdlog%*\"}"
@@ -23,10 +21,12 @@
 #   mpdlog="/var/log/mpd/mpd.log"  # manually code the location if /etc/mpd.conf doesn't exist.
 #fi
 
-. /home/bvargo/bin/mpdignore.functions
-#. /usr/local/bin/mpdignore.functions
-#. /usr/local/bin/mpdignore.functions
-getmpdlog
-getmpdpass
+. "$XDG_CONFIG_HOME/mpd-local.conf"
+. mpdignore.functions
 logsong "skipped"
 mpcp next
+
+#skipped="$(mpc -P "$mpdpass" -h "$mpdhost" current -f "%title%")"
+#queued="$(mpc -P "$mpdpass" -h "$mpdhost" queued -f "%title%")"
+#qartist="$(mpc -P "$mpdpass" -h "$mpdhost" queued -f "%artist%")"
+#notify-send -i audio-x-generic "Song Skipped!" "<b>Next song:</b> $queued by $qartist"
