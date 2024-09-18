d8e5f0508672c77e7a0927a6f7b35754de5bafe1
iconoclast hero <iconoclasthero@gmail.com>
2024-09-18 16:48:45 -0400

    update

--- ignore.sh
+++ ignore.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 mpdconf=/etc/mpd.conf
 
+. "$HOME/.config/mpd-local.conf"
 . mpdignore.functions
-
-getmpdpldir
-getmpdconf
 
 printf "Ignoring current song: "
 mpc -f %file%
-mpc current -f %file% >> "$pldir"/.mpdignore.m3u
+mpc current -f %file% >> "$mpdpldir"/.mpdignore.m3u
 
 read -r -p "Do you wish to skip the current song? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
