6e32a5880bdcd422097e7063a8009004ab312266
iconoclasthero <iconoclasthero@gmail.com>
2024-09-23 02:56:00 -0400

    update

--- ignore.sh
+++ ignore.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
+shopt -s extglob
 mpdconf=/etc/mpd.conf
 
-. "$HOME/.config/mpd-local.conf"
+. "$XDG_CONFIG_HOME"/mpd-local.conf || . "$HOME/.config/mpd-local.conf"
 . mpdignore.functions
 
 printf "Ignoring current song: "
 mpc -f %file%
-mpc current -f %file% >> "$mpdpldir"/.mpdignore.m3u
+
+[[ "$mpdpldir" =~ \  ]] && mpdpldir="${mpdpldir// /\\ }"
+
+
+if [[ "$mpdhost" = @(localhost|127.0.0.1) ]]; then
+  mpc current -f %file% >> "$mpdpldir"/.mpdignore.m3u
+else
+  ignored=$(mpc current -f %file%)
+  ignorecmd=$(printf 'echo "%s"|tee -a %s' "$ignored" "$mpdpldir/.mpdignore.m3u")
+#  ssh -p"$sshport" "$mpdhost" sh -c 'printf '%s\n' "$0"|tee -a "$1"/.mpdignore.m3u' "$ignored" "$mpdpldir"
+  ssh -p"$sshport" "$mpdhost" "$ignorecmd"
+fi
 
 read -r -p "Do you wish to skip the current song? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
